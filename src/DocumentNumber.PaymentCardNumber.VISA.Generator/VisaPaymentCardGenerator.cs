fcfa1575994a735ff35f105ad46e83a284242bbe
Nuno Cancelo <nuno.cancelo@polarising.com>
2022-07-05 12:23:22 +0100

    Add Visa document generation

---
+++ src/DocumentNumber.PaymentCardNumber.VISA.Generator/VisaPaymentCardGenerator.cs
@@ -0,0 +1,13 @@
+﻿using DocumentNumber.PaymentCardNumber.Generator.Common.Generators;
+
+namespace DocumentNumber.PaymentCardNumber.VISA.Generator
+{
+  public class VisaPaymentCardGenerator : CreditCardGeneratorBase
+  {
+    public VisaPaymentCardGenerator()
+    {
+      ValidStartNumbers = new[] { "4" };
+      ValidDocumentNumberLengths = new[] { 13, 16 };
+    }
+  }
+}
