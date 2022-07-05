38ad180ff32bc4ee2696378666df0c7c52d81542
Nuno Cancelo <nuno.cancelo@polarising.com>
2022-07-05 13:42:59 +0100

    Add Visa Electron  document generation

---
+++ src/DocumentNumber.PaymentCardNumber.VISAElectron.Generator/VisaElectronPaymentCardGenerator.cs
@@ -0,0 +1,13 @@
+﻿using DocumentNumber.PaymentCardNumber.Generator.Common.Generators;
+
+namespace DocumentNumber.PaymentCardNumber.VISAElectron.Generator
+{
+  public class VisaElectronPaymentCardGenerator : CreditCardGeneratorBase
+  {
+    public VisaElectronPaymentCardGenerator()
+    {
+      ValidStartNumbers = new[] { "4026", "4508", "4844", "4913", "4917", "417500" };
+      ValidDocumentNumberLengths = new[] { 16 };
+    }
+  }
+}
